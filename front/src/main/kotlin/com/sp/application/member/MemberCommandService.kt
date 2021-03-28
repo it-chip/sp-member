71c79dfd3e12079795f6e78552e0bed2f3da3b2d
Jaedoo.Lee <dlwoen9@godo.co.kr>
2021-03-28 22:11:55 +0900

    apply transaction

--- front/src/main/kotlin/com/sp/application/member/MemberCommandService.kt
+++ front/src/main/kotlin/com/sp/application/member/MemberCommandService.kt
@@ -4,6 +4,7 @@ import com.sp.domain.member.*
 import com.sp.presentation.request.*
 import org.jasypt.springsecurity4.crypto.password.*
 import org.springframework.stereotype.*
+import org.springframework.transaction.support.*
 
 /**
  * @author Jaedoo Lee
@@ -13,10 +14,13 @@ import org.springframework.stereotype.*
 class MemberCommandService(
     private val memberDomainService: MemberDomainService,
     private val passwordEncryptor: PasswordEncoder,
+    private val transactionTemplate: TransactionTemplate,
 ) {
 
     suspend fun registerMember(params: MemberRegisterRequest): Long {
-        return memberDomainService.register(params.copy(password = passwordEncryptor.encode(params.password)))
+        return transactionTemplate.execute {
+            memberDomainService.register(params.copy(password = passwordEncryptor.encode(params.password)))
+        }!!
     }
 
 }
