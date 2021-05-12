fb8ba0a9ec0f008b733af261eb355e752daf9650
Jaedoo <dlwoen9@godo.co.kr>
2021-05-12 23:34:02 +0900

    fix: openapi url gateway url로 수정

--- front/build.gradle.kts
+++ front/build.gradle.kts
@@ -1,11 +1,11 @@
-import com.epages.restdocs.apispec.gradle.OpenApi3Extension
+import com.epages.restdocs.apispec.gradle.*
 
 dependencies {
     runtimeOnly("mysql:mysql-connector-java")
 }
 
 configure<OpenApi3Extension> {
-    setServer(rootProject.property("openapi.url") as String + ":8080")
+    setServer(rootProject.property("openapi.url") as String)
     title = "${rootProject.name}-${project.name}"
     version = project.version as String
     description = """
--- internal/build.gradle.kts
+++ internal/build.gradle.kts
@@ -1,3 +1,17 @@
+import com.epages.restdocs.apispec.gradle.*
+
 dependencies {
     runtimeOnly("mysql:mysql-connector-java")
+}
+
+configure<OpenApi3Extension> {
+    setServer(rootProject.property("openapi.url") as String)
+    title = "${rootProject.name}-${project.name}"
+    version = project.version as String
+    description = """
+                  | Member internal API
+                  """.trimMargin()
+    format = "yml"
+    separatePublicApi = true
+    outputFileNamePrefix = "${rootProject.name}-${project.name}"
 }
